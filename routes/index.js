7c395900342c7e9e0d8832bd52d967db27ffad36
Yutong Zhang <zhangyt0704@outlook.com>
2023-12-04 23:03:20 -0800

    update recommendation and chat table

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ module.exports = function (app, router) {
     app.use('/likes', require('./likes.js')(router));
     app.use('/comments', require('./comments.js')(router));
     app.use('/chats', require('./chats.js')(router));
+    app.use('/recommendArtists', require('./recommendArtists.js')(router));
     
     return router;
 };
